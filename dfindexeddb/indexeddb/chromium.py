261ac0e083453a153278524f97b1b3fc0d1f7ab8
sydp <syd.pleno@gmail.com>
2024-02-24 11:50:22 +1100

    Updates for workflow (#5)

--- dfindexeddb/indexeddb/chromium.py
+++ dfindexeddb/indexeddb/chromium.py
@@ -1,27 +1,23 @@
-from __future__ import annotations
-"""
-Copyright 2024 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 # -*- coding: utf-8 -*-
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Parses Chromium IndexedDb structures."""
+from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
 import io
 from typing import Any, BinaryIO, Optional, Tuple, Type, TypeVar, Union
-
-import leveldb
 
 from dfindexeddb import errors
 from dfindexeddb import utils
